a60f332bc4b4ded58745e8e11f6c8efa9f4b501c
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-07-30 23:08:41 -0500

    added animations

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -5,15 +5,19 @@ const Splash = () => {
         <div className={"splash-content"}>
               <div className={"splash-content-img"}></div>
               <div className={"splash-content-description"}>
-                  <h1 className={"splash-content-title"}>Helping Soldiers Transition</h1>
-                  <p>
-                      Mollit laboris adipisicing pariatur ea non ex anim consequat Lorem ea nulla incididunt sit.
-                  </p>
+                  <div className={"splash-content-para-one"}>
+                    <h1 className={"splash-content-title"}>Helping Soldiers Transition</h1>
+                    <p>
+                        Mollit laboris adipisicing pariatur ea non ex anim consequat Lorem ea nulla incididunt sit.
+                    </p>
+                  </div>
                   <br></br>
-                  <h1 className={"splash-content-title"}>New Opportunities</h1>
-                  <p>
-                      Minim anim proident dolor tempor aliqua.Lorem commodo anim cillum et aliquip dolore veniam culpa incididunt commodo Lorem. Velit fugiat laboris exercitation nisi ipsum.
-                  </p>
+                  <div className={"splash-content-para-two"}>
+                    <h1 className={"splash-content-title"}>New Opportunities</h1>
+                    <p>
+                        Minim anim proident dolor tempor aliqua.Lorem commodo anim cillum et aliquip dolore veniam culpa incididunt commodo Lorem. Velit fugiat laboris exercitation nisi ipsum.
+                    </p>
+                  </div>
               </div>
         </div>
     )
